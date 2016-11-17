2429607a17903dc4be328071cef99c5290cf8417
unorthodox <unorthodox@cyberdude.com>
2016-11-17 03:19:11 +0100

    added admin post activity

---
+++ app/src/main/java/ng/iceetech2016/teamui/ixtech/activity/CDNetViewPostActivity.java
@@ -0,0 +1,15 @@
+package ng.iceetech2016.teamui.ixtech.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ng.iceetech2016.teamui.ixtech.R;
+
+public class CDNetViewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cdnet_view_post);
+    }
+}
