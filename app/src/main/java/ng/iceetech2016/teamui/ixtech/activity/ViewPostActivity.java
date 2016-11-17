da09bbec1873324b6e81f5faebbbfd37bf5f635c
unorthodox <unorthodox@cyberdude.com>
2016-11-17 02:40:38 +0100

    added the posts activity

---
+++ app/src/main/java/ng/iceetech2016/teamui/ixtech/activity/ViewPostActivity.java
@@ -0,0 +1,41 @@
+package ng.iceetech2016.teamui.ixtech.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ng.iceetech2016.teamui.ixtech.R;
+
+public class ViewPostActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.fab) FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+}
